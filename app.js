a3db0e6cf4c880dadb77d21aa9d03dc6778cb0dc
Samara <samarabp7@gmail.com>
2023-01-15 19:26:38 -0800

    Added db functionality to todo list

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public")); //imports the static public folder.
 
-var items = [];
 
+//Mongoose.
+mongoose.connect("mongodb+srv://samarabp7:Goose1@cluster0.yp51xla.mongodb.net/todoList?retryWrites=true&w=majority");
+                    //           username:auth                               Database
 
+const itemsSchema = new mongoose.Schema({name: String});
+const Item = mongoose.model("item", itemsSchema);
+
+//Variables
+let items = [];
+
+// Home.
 app.get("/", function(req, res){
 
-    res.render("index", {items: items});
+    Item.find({}, function(err, results) {
+        if (err){
+            console.log(err)
+        } else {
+            res.render("index", {items: results});
+        }
+    });
 });
-
-
-
-
 
 app.post("/", function(req, res){
 
-    var item = req.body.newItem;
-    items.push(item);
+    var tempItem = req.body.newItem;
+    const itemToAdd = new Item({name: tempItem});
+
+    Item.insertMany(itemToAdd, function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Added to DB.");
+        }
+    });
 
     res.redirect("/");
 });
-
 
 
 
